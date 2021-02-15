6d81d3054259aca9bfe1428c0b31f78c46aef261
podlev@gmail.com <4dvgbhf4fr>
2021-02-15 22:19:19 +0300

    add students.ui

--- main.py
+++ main.py
@@ -3,61 +3,80 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QTableWidgetItem, QMainWindow, QWidget
 import sqlite3
 
-con = sqlite3.connect('database.db')
+connect = sqlite3.connect('database.db')
+cursor = connect.cursor()
+
+class StudentWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('students.ui', self)
 
 
 class GroupWindow(QWidget):
     def __init__(self):
         super().__init__()
         uic.loadUi('group.ui', self)
+        self.add_button.clicked.connect(self.addGroup)
+        self.delete_button.clicked.connect(self.delGroup)
         self.loadTable()
 
     def loadTable(self):
-        connect = sqlite3.connect('database.db')
-        cur = connect.cursor()
-        result = cur.execute("""SELECT * FROM groups""").fetchall()
-        self.tableWidget.setColumnCount(len(result[0]) - 1)
+        result = cursor.execute("""SELECT * FROM groups""").fetchall()
+        self.tableWidget.setColumnCount(len(result[0]))
         self.tableWidget.setRowCount(len(result))
-        self.tableWidget.setHorizontalHeaderLabels(('id', 'ФИО', 'Группа'))
+        self.tableWidget.setHorizontalHeaderLabels(('id', 'Группа'))
         self.tableWidget.setVerticalHeaderLabels([str(i[0]) for i in result])
         for i, row in enumerate(result):
-            for j, elem in enumerate(row[1:]):
+            for j, elem in enumerate(row):
                 self.tableWidget.setItem(i, j, QTableWidgetItem(str(elem)))
         self.tableWidget.resizeColumnsToContents()
 
         for elem in result:
             print(elem)
-        connect.close()
+
+    def addGroup(self):
+        cursor.execute("""INSERT INTO groups(group_name) VALUES(?)""", (self.group_edit.text(),))
+        self.group_edit.clear()
+        connect.commit()
+        self.loadTable()
+
+    def delGroup(self):
+        result = self.tableWidget.model().index(self.tableWidget.currentIndex().row(), 0).data()
+        cursor.execute("""DELETE FROM groups WHERE group_id=(?)""", (result,))
+        connect.commit()
+        self.loadTable()
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.students = StudentWindow()
+        self.groups = GroupWindow()
         uic.loadUi('design.ui', self)
         self.test_button.clicked.connect(self.loadTable)
         self.group_button.clicked.connect(self.groupEdit)
+        self.student_button.clicked.connect(self.studentEdit)
 
     def loadTable(self):
-        connect = sqlite3.connect('database.db')
-        cur = connect.cursor()
-        students = cur.execute("""SELECT * FROM students""").fetchall()
-        groups = cur.execute("""SELECT * FROM groups""").fetchall()
+        students = cursor.execute("""SELECT * FROM students""").fetchall()
+        groups = cursor.execute("""SELECT * FROM groups""").fetchall()
         self.tableWidget.setColumnCount(len(students[0]))
         self.tableWidget.setRowCount(len(students))
         self.tableWidget.setHorizontalHeaderLabels(('id', 'ФИО', 'Группа'))
         self.tableWidget.setVerticalHeaderLabels([str(i[0]) for i in students])
         for i, row in enumerate(students):
-            for j, elem in enumerate(row[1:]):
+            for j, elem in enumerate(row):
                 self.tableWidget.setItem(i, j, QTableWidgetItem(str(elem)))
         self.tableWidget.resizeColumnsToContents()
 
         for elem in students:
             print(elem)
-        connect.close()
 
     def groupEdit(self):
-        self.groups = GroupWindow()
         self.groups.show()
+
+    def studentEdit(self):
+        self.students.show()
 
 
 app = QApplication(sys.argv)
