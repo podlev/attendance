b20ad7f3dbdcbe35651f28038aec89d669ba4748
podlev@gmail.com <4dvgbhf4fr>
2021-03-10 10:51:26 +0300

    add students.ui

--- main.py
+++ main.py
@@ -6,33 +6,31 @@ import sqlite3
 connect = sqlite3.connect('database.db')
 cursor = connect.cursor()
 
+
 class StudentWindow(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi('students.ui', self)
+        uic.loadUi('students_design.ui', self)
 
 
 class GroupWindow(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi('group.ui', self)
+        uic.loadUi('group_design.ui', self)
         self.add_button.clicked.connect(self.addGroup)
         self.delete_button.clicked.connect(self.delGroup)
         self.loadTable()
 
     def loadTable(self):
-        result = cursor.execute("""SELECT * FROM groups""").fetchall()
+        result = cursor.execute("""SELECT group_name FROM groups""").fetchall()
         self.tableWidget.setColumnCount(len(result[0]))
         self.tableWidget.setRowCount(len(result))
-        self.tableWidget.setHorizontalHeaderLabels(('id', 'Группа'))
-        self.tableWidget.setVerticalHeaderLabels([str(i[0]) for i in result])
+        self.tableWidget.setHorizontalHeaderLabels(('Группа', ))
+        self.tableWidget.setVerticalHeaderLabels([str(i) for i in range(1, len(result) + 1)])
         for i, row in enumerate(result):
             for j, elem in enumerate(row):
                 self.tableWidget.setItem(i, j, QTableWidgetItem(str(elem)))
         self.tableWidget.resizeColumnsToContents()
-
-        for elem in result:
-            print(elem)
 
     def addGroup(self):
         cursor.execute("""INSERT INTO groups(group_name) VALUES(?)""", (self.group_edit.text(),))
@@ -58,12 +56,14 @@ class MainWindow(QMainWindow):
         self.student_button.clicked.connect(self.studentEdit)
 
     def loadTable(self):
-        students = cursor.execute("""SELECT * FROM students""").fetchall()
-        groups = cursor.execute("""SELECT * FROM groups""").fetchall()
+        students = cursor.execute(
+            """SELECT name, group_name 
+            FROM students, groups 
+            WHERE students.group_id = groups.group_id""").fetchall()
         self.tableWidget.setColumnCount(len(students[0]))
         self.tableWidget.setRowCount(len(students))
-        self.tableWidget.setHorizontalHeaderLabels(('id', 'ФИО', 'Группа'))
-        self.tableWidget.setVerticalHeaderLabels([str(i[0]) for i in students])
+        self.tableWidget.setHorizontalHeaderLabels(('ФИО', 'Группа'))
+        self.tableWidget.setVerticalHeaderLabels([str(i) for i in range(1, len(students) + 1)])
         for i, row in enumerate(students):
             for j, elem in enumerate(row):
                 self.tableWidget.setItem(i, j, QTableWidgetItem(str(elem)))
